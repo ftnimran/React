f36cb644ad991eafde7e472207ea6cbfcd487c99
Imran Ali <imran30599@gmail.com>
2025-11-03 22:27:46 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
